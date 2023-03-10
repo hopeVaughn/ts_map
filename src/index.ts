99a5dd960377b89769b295080b2eabb0559f915f
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-10 13:39:17 -0800

    update comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 /// <reference types="@types/google.maps" />
-// must update all my imports with :
-// import { faker } from "@faker-js/faker";
+// must update all my applicable classes with : import { faker } from "@faker-js/faker";
 import { User } from './User'
 import { Company } from './Company'
 import { CustomMap } from "./CustomMap";

4e95214d51a8020749fe166f4d1b6458ddc75deb
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-10 12:32:31 -0800

    update html file and add google map api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 /// <reference types="@types/google.maps" />
 // must update all my imports with :
 // import { faker } from "@faker-js/faker";
-import { User } from './User'
-import { Company } from './Company'
-const user = new User();
-const company = new Company();
+// import { User } from './User'
+// import { Company } from './Company'
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
+

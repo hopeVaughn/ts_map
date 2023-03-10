028a22b1aab5352abf54c0c7714e59b7654d698c
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-10 12:04:17 -0800

    update dependency and readme

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
+/// <reference types="@types/google.maps" />
 // must update all my imports with :
 // import { faker } from "@faker-js/faker";
 import { User } from './User'
 import { Company } from './Company'
 const user = new User();
 const company = new Company();
-console.log(company);

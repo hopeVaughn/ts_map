90ab78af4654699f44cbd1c9dcc2a48df1ce5580
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-10 11:45:08 -0800

    create company class and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 // must update all my imports with :
 // import { faker } from "@faker-js/faker";
 import { User } from './User'
+import { Company } from './Company'
+const user = new User();
+const company = new Company();
+console.log(company);

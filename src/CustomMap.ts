81db70c8151b511397b8d0e27a914c726c6fd8c2
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-10 13:03:26 -0800

    add bad code example for Map class

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,5 @@
+import { User } from './User'
+import { Company } from './Company'
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -9,5 +11,25 @@ export class CustomMap {
         lng: 0
       }
     });
+  }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    })
+    //
+  }
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 /// <reference types="@types/google.maps" />
 // must update all my imports with :
 // import { faker } from "@faker-js/faker";
-// import { User } from './User'
-// import { Company } from './Company'
+import { User } from './User'
+import { Company } from './Company'
 import { CustomMap } from "./CustomMap";
-new CustomMap('map');
+const user = new User;
+const company = new Company;
+const customMap = new CustomMap('map');
 
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
